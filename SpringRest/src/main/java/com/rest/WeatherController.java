a634cb7c0b2eb7b5b3994d78155823a2d5de57e5
k3shav17 <keshavrao0489@gmail.com>
2022-03-23 12:46:28 +0530

    spring with restful, 💭weather apis

---
+++ SpringRest/src/main/java/com/rest/WeatherController.java
@@ -0,0 +1,23 @@
+package com.rest;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@RestController
+public class WeatherController {
+
+	@GetMapping("/byCity/{country}/{city}")
+	public @ResponseBody Object getWeatherByCity(@PathVariable String city, @PathVariable String country) {
+
+		RestTemplate restTemplate = new RestTemplate();
+
+		ResponseEntity<Object> response = restTemplate.getForEntity("https://api.openweathermap.org/data/2.5/weather?q="
+				+ city + "&appid=df2934df5409500c5ff7aa24a94b81f6", Object.class);
+		return response;
+	}
+
+}
