4d2980c50a5dab3573a8ada98682c9fb704538cc
k3shav17 <keshavrao0489@gmail.com>
2022-03-25 11:05:41 +0530

    added request api to get the weather in celsius

--- SpringRest/src/main/java/com/rest/WeatherController.java
+++ SpringRest/src/main/java/com/rest/WeatherController.java
@@ -15,8 +15,35 @@ public class WeatherController {
 
 		RestTemplate restTemplate = new RestTemplate();
 
-		ResponseEntity<Object> response = restTemplate.getForEntity("https://api.openweathermap.org/data/2.5/weather?q="
-				+ city + "&appid=API_KEY", Object.class);
+		ResponseEntity<Object> response = restTemplate.getForEntity(
+				"https://api.openweathermap.org/data/2.5/weather?q=" + city + "API_KEY",
+				Object.class);
+		return response;
+	}
+
+	@GetMapping("/forecastByCity/{country}/{city}")
+	public @ResponseBody Object getForecastByCity(@PathVariable String city, @PathVariable String code) {
+		RestTemplate restTemplate = new RestTemplate();
+
+		ResponseEntity<Object> response = restTemplate
+				.getForEntity("https://api.openweathermap.org/data/2.5/forecast?q=" + city
+						+ "API_KEY", Object.class);
+		return response;
+	}
+
+	@GetMapping("/byCity/{country}/{city}/{units}")
+	public @ResponseBody Object getWeatherByCelcius(@PathVariable String country, @PathVariable String city,
+			@PathVariable String units) {
+
+		/*
+		 * "lon": 78.4744, "lat": 17.3753
+		 */
+
+		RestTemplate restTemplate = new RestTemplate();
+
+		ResponseEntity<Object> response = restTemplate.getForEntity("https://api.openweathermap.org/data/2.5/onecall?"
+				+ city + "&units=metric&API_KEY", Object.class);
+
 		return response;
 	}
 
